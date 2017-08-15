3db1c6e89f595546882e4e5f1e3432d6e860fab8
Roman Niukhalov <roman.niukhalov@agoda.com>
2017-08-15 16:03:02 +0700

    Fixed update entity

--- src/main/scala/com/github/nyukhalov/highloadcup/web/actor/UpdateLocationActor.scala
+++ src/main/scala/com/github/nyukhalov/highloadcup/web/actor/UpdateLocationActor.scala
@@ -2,7 +2,7 @@ package com.github.nyukhalov.highloadcup.web.actor
 
 import akka.actor.Actor
 import com.github.nyukhalov.highloadcup.core.AppLogger
-import com.github.nyukhalov.highloadcup.core.domain.{Location, User}
+import com.github.nyukhalov.highloadcup.core.domain.Location
 import com.github.nyukhalov.highloadcup.database.DB
 import com.github.nyukhalov.highloadcup.web.domain._
 
@@ -15,34 +15,32 @@ class UpdateLocationActor extends Actor with AppLogger {
     case UpdateLocation(id, locationUpdate) =>
       val to = sender()
 
-      to ! SuccessfulOperation
+      DB.findLocationById(id).onComplete {
+        case Success(res) =>
+          res match {
+            case None =>
+              to ! NotExist(s"Location with id $id does not exist")
 
-//      DB.findLocationById(id).onComplete {
-//        case Success(res) =>
-//          res match {
-//            case None =>
-//              to ! NotExist(s"Location with id $id does not exist")
-//
-//            case Some(l) =>
-//              val updatedLocation = Location(
-//                id,
-//                locationUpdate.place.getOrElse(l.place),
-//                locationUpdate.country.getOrElse(l.country),
-//                locationUpdate.city.getOrElse(l.city),
-//                locationUpdate.distance.getOrElse(l.distance)
-//              )
-//
-//              DB.updateLocation(updatedLocation).onComplete {
-//                case Success(_) =>
-//                  to ! SuccessfulOperation
-//
-//                case Failure(ex) =>
-//                  to ! Error(ex.getMessage)
-//              }
-//          }
-//
-//        case Failure(ex) =>
-//          to ! Error(ex.getMessage)
-//      }
+            case Some(l) =>
+              val updatedLocation = Location(
+                id,
+                locationUpdate.place.getOrElse(l.place),
+                locationUpdate.country.getOrElse(l.country),
+                locationUpdate.city.getOrElse(l.city),
+                locationUpdate.distance.getOrElse(l.distance)
+              )
+
+              DB.updateLocation(updatedLocation).onComplete {
+                case Success(_) =>
+                  to ! SuccessfulOperation
+
+                case Failure(ex) =>
+                  to ! Error(ex.getMessage)
+              }
+          }
+
+        case Failure(ex) =>
+          to ! Error(ex.getMessage)
+      }
   }
 }
--- src/main/scala/com/github/nyukhalov/highloadcup/web/actor/UpdateVisitActor.scala
+++ src/main/scala/com/github/nyukhalov/highloadcup/web/actor/UpdateVisitActor.scala
@@ -2,7 +2,7 @@ package com.github.nyukhalov.highloadcup.web.actor
 
 import akka.actor.Actor
 import com.github.nyukhalov.highloadcup.core.AppLogger
-import com.github.nyukhalov.highloadcup.core.domain.{Location, Visit}
+import com.github.nyukhalov.highloadcup.core.domain.Visit
 import com.github.nyukhalov.highloadcup.database.DB
 import com.github.nyukhalov.highloadcup.web.domain._
 
@@ -15,34 +15,32 @@ class UpdateVisitActor extends Actor with AppLogger {
     case UpdateVisit(id, visitUpdate) =>
       val to = sender()
 
-      to ! SuccessfulOperation
+      DB.findVisitById(id).onComplete {
+        case Success(res) =>
+          res match {
+            case None =>
+              to ! NotExist(s"Visit with id $id does not exist")
 
-//      DB.findVisitById(id).onComplete {
-//        case Success(res) =>
-//          res match {
-//            case None =>
-//              to ! NotExist(s"Visit with id $id does not exist")
-//
-//            case Some(v) =>
-//              val updatedVisit = Visit(
-//                id,
-//                visitUpdate.location.getOrElse(v.location),
-//                visitUpdate.user.getOrElse(v.user),
-//                visitUpdate.visitedAt.getOrElse(v.visitedAt),
-//                visitUpdate.mark.getOrElse(v.mark)
-//              )
-//
-//              DB.updateVisit(updatedVisit).onComplete {
-//                case Success(_) =>
-//                  to ! SuccessfulOperation
-//
-//                case Failure(ex) =>
-//                  to ! Error(ex.getMessage)
-//              }
-//          }
-//
-//        case Failure(ex) =>
-//          to ! Error(ex.getMessage)
-//      }
+            case Some(v) =>
+              val updatedVisit = Visit(
+                id,
+                visitUpdate.location.getOrElse(v.location),
+                visitUpdate.user.getOrElse(v.user),
+                visitUpdate.visitedAt.getOrElse(v.visitedAt),
+                visitUpdate.mark.getOrElse(v.mark)
+              )
+
+              DB.updateVisit(updatedVisit).onComplete {
+                case Success(_) =>
+                  to ! SuccessfulOperation
+
+                case Failure(ex) =>
+                  to ! Error(ex.getMessage)
+              }
+          }
+
+        case Failure(ex) =>
+          to ! Error(ex.getMessage)
+      }
   }
 }

2d2c551c02c42cba28f7e6b131f5f6c13fe6e59b
Roman Niukhalov <roman.niukhalov@agoda.com>
2017-08-20 17:09:57 +0700

    Added fast cache warmup

--- src/main/scala/com/github/nyukhalov/highloadcup/web/RapidoidHttpServer.scala
+++ src/main/scala/com/github/nyukhalov/highloadcup/web/RapidoidHttpServer.scala
@@ -1,12 +1,15 @@
 package com.github.nyukhalov.highloadcup.web
 
+import java.util.concurrent.ConcurrentHashMap
+
 import com.github.nyukhalov.highloadcup.core.domain.{Location, User, Visit}
-import com.github.nyukhalov.highloadcup.core.{DataLoader, HLService, HLServiceImpl}
+import com.github.nyukhalov.highloadcup.core.{AppLogger, DataLoader, HLService, HLServiceImpl}
 import com.github.nyukhalov.highloadcup.web.domain._
 import com.github.nyukhalov.highloadcup.web.json.JsonSupport
 import io.circe.parser._
 import org.rapidoid.buffer.Buf
 import org.rapidoid.bytes.BytesUtil
+import org.rapidoid.data.JSON
 import org.rapidoid.http._
 import org.rapidoid.http.impl.PathPattern
 import org.rapidoid.net.abstracts.Channel
@@ -16,7 +19,8 @@ import scala.collection.JavaConverters._
 import scala.collection.mutable
 import scala.util.Try
 
-class RapidoidHttpServer(serverPort: Int, hlService: HLService) extends AbstractHttpServer with JsonSupport with HttpServer {
+class RapidoidHttpServer(serverPort: Int, hlService: HLService)
+  extends AbstractHttpServer with JsonSupport with HttpServer with AppLogger {
   private val HTTP_400 = fullResp(400, "nf".getBytes())
 
   private val EMPTY_JSON = "{}".getBytes()
@@ -27,6 +31,7 @@ class RapidoidHttpServer(serverPort: Int, hlService: HLService) extends Abstract
   private val entityMethodPattern = PathPattern.from("/{entity}/{id}/{method}")
   private val entityPattern = PathPattern.from("/{entity}/{id}")
 
+  private val cache: mutable.Map[String, Array[Byte]] = new ConcurrentHashMap[String, Array[Byte]]().asScala
 
   override def start(): Unit = {
     listen(serverPort)
@@ -44,10 +49,11 @@ class RapidoidHttpServer(serverPort: Int, hlService: HLService) extends Abstract
     HttpStatus.ASYNC
   }
 
-  private def handleEntityRequest(ctx: Channel, buf: Buf, req: RapidoidHelper, params: mutable.Map[String, String]) = {
+  private def handleEntityRequest(uri: String, ctx: Channel, buf: Buf, req: RapidoidHelper, params: mutable.Map[String, String]) = {
     val entity = params("entity")
     val strId = params("id")
     val idOpt = parseId(strId)
+    var flushCache = false
 
     val resp = if (strId == "new") {
       entity match {
@@ -64,6 +70,7 @@ class RapidoidHttpServer(serverPort: Int, hlService: HLService) extends Abstract
         case ("visits") =>
           if (req.isGet.value) NotExist
           else {
+            flushCache = true
             val body = BytesUtil.get(buf.bytes(), req.body)
             decode[Visit](body) match {
               case Left(_) => Validation
@@ -93,6 +100,7 @@ class RapidoidHttpServer(serverPort: Int, hlService: HLService) extends Abstract
         case "users" =>
           if (req.isGet.value) hlService.getUser(id)
           else {
+            flushCache = true
             val body = BytesUtil.get(buf.bytes(), req.body)
             decode[UserUpdate](body) match {
               case Left(_) => Validation
@@ -103,6 +111,7 @@ class RapidoidHttpServer(serverPort: Int, hlService: HLService) extends Abstract
         case "visits" =>
           if (req.isGet.value) hlService.getVisit(id)
           else {
+            flushCache = true
             val body = BytesUtil.get(buf.bytes(), req.body)
             decode[VisitUpdate](body) match {
               case Left(_) => Validation
@@ -113,6 +122,7 @@ class RapidoidHttpServer(serverPort: Int, hlService: HLService) extends Abstract
         case "locations" =>
           if (req.isGet.value) hlService.getLocation(id)
           else {
+            flushCache = true
             val body = BytesUtil.get(buf.bytes(), req.body)
             decode[LocationUpdate](body) match {
               case Left(_) => Validation
@@ -122,7 +132,7 @@ class RapidoidHttpServer(serverPort: Int, hlService: HLService) extends Abstract
       }
     }
 
-    toResponse(ctx, req, resp)
+    toResponse(uri, ctx, req, resp, flushCache)
   }
 
   private def getParams(buf: Buf, req: RapidoidHelper) = {
@@ -139,7 +149,7 @@ class RapidoidHttpServer(serverPort: Int, hlService: HLService) extends Abstract
     }
   }
 
-  private def handleEntityMethodRequest(ctx: Channel, buf: Buf, req: RapidoidHelper, params: mutable.Map[String, String]) = {
+  private def handleEntityMethodRequest(uri: String, ctx: Channel, buf: Buf, req: RapidoidHelper, params: mutable.Map[String, String]) = {
     val method = params("method")
     val entity = params("entity")
     val strId = params("id")
@@ -190,34 +200,61 @@ class RapidoidHttpServer(serverPort: Int, hlService: HLService) extends Abstract
         case _ => NotExist
       }
     }
-    toResponse(ctx, req, resp)
+    toResponse(uri, ctx, req, resp)
   }
 
-  private def toResponse(ctx: Channel, req: RapidoidHelper, resp: Any): HttpStatus = {
+  private def toResponse(uri: String, ctx: Channel, req: RapidoidHelper, resp: Any, flushCache: Boolean = false): HttpStatus = {
     resp match {
       case NotExist => HttpStatus.NOT_FOUND
       case Validation => badRequest(ctx, req)
-      case SuccessfulOperation => ok(ctx, req.isKeepAlive.value, EMPTY_JSON, MediaType.APPLICATION_JSON)
+      case SuccessfulOperation => {
+        if (!req.isGet.value && flushCache) {
+          cache.remove(uri)
+        }
+        ok(ctx, req.isKeepAlive.value, EMPTY_JSON, MediaType.APPLICATION_JSON)
+      }
       case x => {
-        serializeToJson(HttpUtils.noReq(), ctx, req.isKeepAlive.value, x)
+        val bytes = JSON.stringifyToBytes(x)
+        if (req.isGet.value) {
+          cache += (uri -> bytes)
+        }
+        json(ctx, req.isKeepAlive.value, bytes)
       }
     }
   }
 
   override def handle(ctx: Channel, buf: Buf, req: RapidoidHelper): HttpStatus = {
+    val uri = BytesUtil.get(buf.bytes(), req.uri)
 
+    if (req.isGet.value) {
+      val resp = cache.get(uri)
+      if (resp.isDefined) {
+        json(ctx, req.isKeepAlive.value, resp.get)
+      } else {
+        handleReq(uri, ctx, buf, req)
+      }
+    } else {
+      handleReq(uri, ctx, buf, req)
+    }
+  }
+
+  private def handleReq(uri: String, ctx: Channel, buf: Buf, req: RapidoidHelper): HttpStatus = {
     val path = BytesUtil.get(buf.bytes(), req.path)
 
-    var params = Option(entityPattern.`match`(path) asScala)
+    var params = Option(entityPattern.`match`(path).asScala)
     if (params.isDefined) {
-      handleEntityRequest(ctx, buf, req, params.get)
+      handleEntityRequest(uri, ctx, buf, req, params.get)
     } else {
-      params = Option(entityMethodPattern.`match`(path) asScala)
+      params = Option(entityMethodPattern.`match`(path).asScala)
       if (params.isDefined) {
-        handleEntityMethodRequest(ctx, buf, req, params.get)
+        handleEntityMethodRequest(uri, ctx, buf, req, params.get)
       } else {
         HttpStatus.NOT_FOUND
       }
     }
+  }
+
+  def warmupCache(uri: String, responseObj: Any): Unit = {
+    cache += (uri -> JSON.stringifyToBytes(responseObj))
   }
 }
--- src/main/scala/com/github/nyukhalov/highloadcup/web/WebBoot.scala
+++ src/main/scala/com/github/nyukhalov/highloadcup/web/WebBoot.scala
@@ -1,26 +1,62 @@
 package com.github.nyukhalov.highloadcup.web
 
+import java.util.concurrent.{Executors, TimeUnit}
+
+import better.files.File
 import com.github.nyukhalov.highloadcup.core.{AppLogger, DataLoader, HLServiceImpl}
 import com.typesafe.config.ConfigFactory
-import org.rapidoid.http.HTTP
+import org.rapidoid.http.{HTTP, HttpClient}
+
+import scala.concurrent.duration._
 
 object WebBoot extends AppLogger {
   def run(): Unit = {
     val config = ConfigFactory.load()
 
     val pathToZip = config.getString("datazip.path")
+    val pathToOption = config.getString("option.path")
     val serverPort = config.getInt("server.port")
 
     val hlService = new HLServiceImpl()
     new DataLoader(hlService).loadData(pathToZip)
+    val warmupTime = getWarmupTime(pathToOption)
 
-    new RapidoidHttpServer(serverPort, hlService).start()
+    val server = new RapidoidHttpServer(serverPort, hlService)
+    server.start()
 
-    val c = HTTP.client()
-    c.req().get(s"http://localhost:$serverPort/users/1")
-    c.req().get(s"http://localhost:$serverPort/visits/1")
-    c.req().get(s"http://localhost:$serverPort/users/1")
-    c.req().get(s"http://localhost:$serverPort/users/1/visits")
-    c.req().get(s"http://localhost:$serverPort/locations/1/avg")
+    val s = System.currentTimeMillis()
+
+    hlService.userMap.foreach{ case (id, user) => {
+      server.warmupCache(s"/users/$id", user)
+      server.warmupCache(s"/users/$id/visits", hlService.getUserVisits(id, None, None, None, None))
+    }}
+    hlService.locMap.foreach{ case (id, loc) => {
+      server.warmupCache(s"/locations/$id", loc)
+      server.warmupCache(s"/locations/$id/avg", hlService.getAverageRating(id, None, None, None, None, None))
+    }}
+    hlService.visitMap.foreach { case (id, visit) => {
+      server.warmupCache(s"/visits/$id", visit)
+    }}
+    logger.info(s"Warmed up for ${System.currentTimeMillis() - s} ms")
+  }
+
+  private def getWarmupTime(pathToOption: String) = {
+    val TRAIN_TIME = 30.seconds
+    val FULL_TIME = 3.minutes
+
+    val option = File(pathToOption)
+    if (!option.exists) {
+      logger.warn("Can not find option file $pathToOption. User train time")
+      TRAIN_TIME
+    } else {
+      val mode = option.lines().toList(1).toInt
+      if (mode == 1) {
+        logger.info(s"FULL mode ($FULL_TIME warmup)")
+        FULL_TIME
+      } else {
+        logger.info(s"TRAIN mode ($TRAIN_TIME warmup)")
+        TRAIN_TIME
+      }
+    }
   }
 }
